b457bfe761b30b4118088e882871aeb9bfacaaea
MinexAutomation <minex.automation@gmail.com>
2019-07-21 19:26:44 -0400

    Added types for specifying solution build configurations.

---
+++ source/R5T.Code.VisualStudio.Types.Core/Code/Classes/SolutionBuildConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace R5T.Code.VisualStudio
+{
+    /// <summary>
+    /// A combination of build configuration and platform target specifies a solution build configuration.
+    /// </summary>
+    public class SolutionBuildConfiguration
+    {
+        public BuildConfiguration BuildConfiguration { get; set; }
+        public PlatformTarget PlatformTarget { get; set; }
+    }
+}
