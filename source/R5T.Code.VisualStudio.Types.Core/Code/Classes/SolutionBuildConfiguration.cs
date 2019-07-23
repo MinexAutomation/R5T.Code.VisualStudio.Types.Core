d6b336325a5ed48c6bcb1c563816171d6c27ea95
MinexAutomation <minex.automation@gmail.com>
2019-07-23 16:00:20 -0400

    Added {Debug, Release} x {AnyCPU, x64, and x86} solution build singleton instances for ease of use (no need to create these instances in multiple times).

--- source/R5T.Code.VisualStudio.Types.Core/Code/Classes/SolutionBuildConfiguration.cs
+++ source/R5T.Code.VisualStudio.Types.Core/Code/Classes/SolutionBuildConfiguration.cs
@@ -8,6 +8,14 @@ namespace R5T.Code.VisualStudio
     /// </summary>
     public class SolutionBuildConfiguration
     {
+        public static readonly SolutionBuildConfiguration DebugAnyCPU = new SolutionBuildConfiguration { BuildConfiguration = BuildConfiguration.Debug, PlatformTarget = PlatformTarget.AnyCPU };
+        public static readonly SolutionBuildConfiguration DebugX64 = new SolutionBuildConfiguration { BuildConfiguration = BuildConfiguration.Debug, PlatformTarget = PlatformTarget.x64 };
+        public static readonly SolutionBuildConfiguration DebugX86 = new SolutionBuildConfiguration { BuildConfiguration = BuildConfiguration.Debug, PlatformTarget = PlatformTarget.x32 };
+        public static readonly SolutionBuildConfiguration ReleaseAnyCPU = new SolutionBuildConfiguration { BuildConfiguration = BuildConfiguration.Release, PlatformTarget = PlatformTarget.AnyCPU };
+        public static readonly SolutionBuildConfiguration ReleaseX64 = new SolutionBuildConfiguration { BuildConfiguration = BuildConfiguration.Release, PlatformTarget = PlatformTarget.x64 };
+        public static readonly SolutionBuildConfiguration ReleaseX86 = new SolutionBuildConfiguration { BuildConfiguration = BuildConfiguration.Release, PlatformTarget = PlatformTarget.x32 };
+
+
         public BuildConfiguration BuildConfiguration { get; set; }
         public PlatformTarget PlatformTarget { get; set; }
     }
